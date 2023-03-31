528e737dc87f7220eec5522de971b858c037fc64
costawhitehat <95866232+costawhitehat@users.noreply.github.com>
2023-03-31 10:48:44 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.post('/', function(req, res) {
+  console.log('Got a POST request');
+  res.send('Received POST request');
+});
+
+app.use(express.static('public'));
+
+app.get('/user/:id', function(req, res) {
+  console.log('Got a request for user ID:', req.params.id);
+  res.send('User ID: ' + req.params.id);
+});
+
+app.listen(3000, function() {
+  console.log('Server listening on port 3000');
+});
